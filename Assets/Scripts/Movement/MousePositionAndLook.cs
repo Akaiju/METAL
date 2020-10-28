c6f83b16978f158804f9b048b73c20a5067802f9
Akaiju <jackbuchaniec@gmail.com>
2020-10-27 22:37:17 -0500

    Imported custom cursor, created MousePositionAndLook script (WIP)

---
+++ Assets/Scripts/Movement/MousePositionAndLook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePositionAndLook : MonoBehaviour
+{
+    Vector3 worldPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        worldPosition = Camera.main.ScreenToViewportPoint(mousePos);
+    }
+}
