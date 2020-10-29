1ccc79aa2f22f663f317c15d59726afa1f42183c
Akaiju <jackbuchaniec@gmail.com>
2020-10-29 13:43:58 -0500

    Added new experimental code to MousePositionAndLook

--- Assets/Scripts/Movement/MousePositionAndLook.cs
+++ Assets/Scripts/Movement/MousePositionAndLook.cs
@@ -5,38 +5,50 @@ using UnityEngine;
 
 public class MousePositionAndLook : MonoBehaviour
 {
+    // speed is the rate at which the object will rotate
+    public float speed;
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
     }
 
-    private void Update()
+    void FixedUpdate()
     {
-        //Mouse Position in the world. It's important to give it some distance from the camera. 
-        //If the screen point is calculated right from the exact position of the camera, then it will
-        //just return the exact same position as the camera, which is no good.
-        //Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10f);
-
-        //Angle between mouse and this object
-        //float angle = AngleBetweenPoints(transform.position, mouseWorldPosition);
-
-        //Ta daa
-        //transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
-
         Vector3 mousePos = Input.mousePosition;
         Debug.Log($"mousePosition: {mousePos}");
 
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main.nearClipPlane));
         Debug.Log($"point: {worldPoint}");
 
-        //mousePos.z = Camera.main.nearClipPlane;
-        //worldPosition = Camera.main.ScreenToViewportPoint(mousePos);
+        // Generate a plane that intersects the transform's position with an upwards normal.
+        Plane playerPlane = new Plane(Vector3.up, transform.position);
+
+        // Generate a ray from the cursor position
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Determine the point where the cursor ray intersects the plane.
+        // This will be the point that the object must look towards to be looking at the mouse.
+        // Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+        //   then find the point along that ray that meets that distance.  This will be the point
+        //   to look at.
+        float hitdist = 16.0f;
+        // If the ray is parallel to the plane, Raycast will return false.
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            // Get the point along the ray that hits the calculated distance.
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+
+            // Determine the target rotation.  This is the rotation if the transform looks at the target point.
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+            // Smoothly rotate towards the target point.
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+        }
     }
 
     float AngleBetweenPoints(Vector2 a, Vector2 b)
     {
         return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
     }
-
-    
 }
