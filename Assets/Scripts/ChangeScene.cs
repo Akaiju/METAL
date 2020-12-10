11b33e04cd8db98177d90a880cd39666ef1650fc
Akaiju <jackbuchaniec@gmail.com>
2020-12-10 14:39:47 -0600

    ChangeScene script refactored, now functions!

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -8,16 +8,17 @@ public class ChangeScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.tag == "Player")
-                SceneManager.LoadScene(SampleScene);
-        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnCollisionEnter(Collision gameObjectInformation)
+    {
+        if (gameObjectInformation.gameObject.tag == "Player")
+            SceneManager.LoadScene("SampleScene");
     }
 }
