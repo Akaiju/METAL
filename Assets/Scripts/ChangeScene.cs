fa08cc00aa00e392a8706d5f7794a06e153bf0dc
Akaiju <jackbuchaniec@gmail.com>
2020-12-09 22:52:33 -0600

    Changed tutorialscene geometry somewhat, added changescene script

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        void OnCollisionEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+                SceneManager.LoadScene(2);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
